4c8c516ca988e0d9e792e9abf462ba9277469e08
Eric Londaits <eric.londaits@gmail.com>
2025-09-08 12:26:51 -0300

    design: Adds new tiles for residential areas, industrial areas, solar farms and livestock.

--- extras/src/js/lib/init/inject-tile-renderers.js
+++ extras/src/js/lib/init/inject-tile-renderers.js
@@ -8,6 +8,10 @@ function injectTileRenderers(config, mapView) {
   const wetlandsTileId = getTileTypeId(config, 'wetlands');
   const livestockTileId = getTileTypeId(config, 'livestock');
   const solarFarmTileId = getTileTypeId(config, 'solar-farm');
+  const industrialTileId = getTileTypeId(config, 'industrial');
+  const urbanHighTileId = getTileTypeId(config, 'urban-high');
+  const urbanMidTileId = getTileTypeId(config, 'urban-mid');
+  const urbanLowTileId = getTileTypeId(config, 'urban-low');
   const waterTileId = getTileTypeId(config, 'water');
 
   mapView.addTileTypeRenderer(cropsTileId, new OrientedTextureTileRenderer(mapView, 'landscape', 'crops'));
@@ -15,6 +19,10 @@ function injectTileRenderers(config, mapView) {
   mapView.addTileTypeRenderer(wetlandsTileId, new RandomTextureTileRenderer(mapView, 'landscape', 'wetlands', 4));
   mapView.addTileTypeRenderer(livestockTileId, new RandomTextureTileRenderer(mapView, 'landscape', 'livestock', 1, true));
   mapView.addTileTypeRenderer(solarFarmTileId, new RandomTextureTileRenderer(mapView, 'landscape', 'solar-farm', 1, true));
+  mapView.addTileTypeRenderer(industrialTileId, new RandomTextureTileRenderer(mapView, 'landscape', 'industrial', 1, true));
+  mapView.addTileTypeRenderer(urbanHighTileId, new RandomTextureTileRenderer(mapView, 'landscape', 'urban-high', 1, true));
+  mapView.addTileTypeRenderer(urbanMidTileId, new RandomTextureTileRenderer(mapView, 'landscape', 'urban-mid', 1, true));
+  mapView.addTileTypeRenderer(urbanLowTileId, new RandomTextureTileRenderer(mapView, 'landscape', 'urban-low', 1, true));
   mapView.addTileTypeRenderer(waterTileId, new RandomTextureTileRenderer(mapView, 'water', 'water', 8));
 }
 
